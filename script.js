7b8a1779c870c910fc9ca7f24243b6dfd97cf0f1
Yuireising <yuireisingngalung2000@gmail.com>
2025-05-01 18:20:53 +0530

    new files added

---
+++ script.js
@@ -0,0 +1,7 @@
+function closeSidemenu(){
+    document.getElementById("sidebar__close").style.display="none";
+}
+
+function openSidemenu(){
+    document.getElementById("sidebar__close").style.display="block";
+}
